cc1d3451f35ed7619c36ee15d55813114228317e
Adam Spanswick <aspanswick@unm.edu>
2017-10-12 05:47:58 +0300

    Version 4 done. Class and method comments added.

--- src/Player.java
+++ src/Player.java
@@ -9,7 +9,7 @@ import java.util.ArrayList;
 public class Player
 {
   private ArrayList<String> guessedWords;
-//  private int score;
+  private int score;
 
   //********************************************************************************************************************
   //Parameters: none
